d9e538b5f00ea399678adc683d2c43bfc51afdcb
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-07-30 17:53:18 +0530

    Add Function to reverse the string
     + Created a function that takes "string" as a parameter.
     + Used the string methods such as `string.length` and `string.at()` to achieve the desired output.
     + If the `string === ""`, it will return `""`.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let newString = "";
+  let length = string.length - 1;
 
+  if (string === "") {
+    return newString;
+  }
+  do {
+    newString += string.at(length);
+    length--;
+  } while (length >= 0);
+
+  return newString;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
