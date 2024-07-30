f94be735a50401d1835882e4b21d2b9b0d8bba10
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-07-30 16:49:16 +0530

    Add Function to repeat strings
    + Created a function with "string" and "number" as parameters for the function.
    + If the `number === 0` returns `""`.
    + If the `number < 0` returns `ERROR`.
    + Else the function will return the string equals the 'number' times.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,30 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  let newString;
 
+  if (number === 0) {
+    return (newString = "");
+  }
+
+  if (number < 0) {
+    return (newString = "ERROR");
+  }
+
+  for (let i = 1; i <= number; i++) {
+    if (newString === undefined) {
+      newString = string;
+    } else {
+      newString += string;
+    }
+  }
+  return newString;
 };
+
+repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("goodbye", -1);
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
