7e47a1465305be3554ac4268f29fc5990eef588e
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-07-30 22:23:30 +0530

    Add Function to remove elements from array
     + Created a function with "rest parameters" `...args` in which the function can accept multiple arguments as an array.
     + Calculated the length of the parameter and checks whether the argument is present in the array.
     + If the argument is present in the array the element is removed and recheck till every similar element is removed.
     + The array methods such as `array.length`, `array.includes()`, `array.indexOf()`, `array.splice()` was used in the function.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeElement) {
+  let length = removeElement.length - 1;
+
+  while (length >= 0) {
+    let argument = removeElement[length];
+
+    while (array.includes(argument)) {
+      let index = array.indexOf(argument);
+
+      array.splice(index, 1);
+    }
+    length--;
+  }
+
+  return array;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
