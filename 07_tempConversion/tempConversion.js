a3ba6c876a74d2c8a3eb342b914ea1d4e553e8c6
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-07-31 19:26:53 +0530

    Add Function to convert scales of temperature
     + Created a function that takes "fahrenheit" as a parameter and returns "celsius".
     + Formula for fahrenheit --> celsius = "(x - 32) * 5/9"
     + Created a function that takes "celsius" as a parameter and returns "fahrenheit".
     + Formula for celsius --> fahrenheit = "(x * 9/5) + 32"
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  let fahrenheit;
+  fahrenheit = ((degree - 32) * 5) / 9;
+
+  return Number(fahrenheit.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  let celsius;
+
+  celsius = (degree * 9) / 5 + 32;
+
+  return Number(celsius.toFixed(1));
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
