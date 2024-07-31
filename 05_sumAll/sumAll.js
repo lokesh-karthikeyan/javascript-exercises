9940af850a932fa4e7f89f31677a05fa1c2085d9
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-07-31 13:40:02 +0530

    Add Function to sum numbers between the args
     + Created a function to sum all the numbers inbetween and including the parameters.
     + Other than "number" types, such as "strings", "arrays", "objects" will return an "Error".
     + "Negative integers" and "Decimal integers" will also return an "Error".
     + It can also perform the sum, even if the parameters are in reversed order. For eg: (10, 1).
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function (firstOperand, secondOperand) {
+  let sum = 0;
 
+  if (
+    Math.sign(firstOperand) !== -1 &&
+    Math.sign(secondOperand) !== -1 &&
+    Number.isInteger(firstOperand) &&
+    Number.isInteger(secondOperand)
+  ) {
+    if (firstOperand > secondOperand) {
+      let temp = firstOperand;
+      firstOperand = secondOperand;
+      secondOperand = temp;
+    }
+    for (let i = firstOperand; i <= secondOperand; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
+
+sumAll(1, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(2.5, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
