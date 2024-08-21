7e604dc65e49623a2e0d5acfa34e20e6948b8f0e
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-08-21 21:32:42 +0530

    Add functions to perform calculations
     + Created functions for "Addition", "Subtraction", "Multiplication" of the operands.
     + Functions performing "Power", "Factorial", "Sum of the total data items inside the array" are also created.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,57 @@
-const add = function() {
-	
+const add = function (operandOne, operandTwo) {
+  return operandOne + operandTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (operandOne, operandTwo) {
+  return operandOne - operandTwo;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce(
+    (accumulated, currentValue) => (accumulated += currentValue),
+    0,
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce(
+    (accumulated, currentValue) => (accumulated *= currentValue),
+    1,
+  );
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
 };
+
+add(0, 0);
+add(2, 2);
+add(2, 6);
+
+subtract(10, 4);
+
+sum([]);
+sum([7]);
+sum([7, 11]);
+sum([1, 3, 5, 7, 9]);
+
+multiply([2, 4]);
+multiply([2, 4, 6, 8, 10, 12, 14]);
+
+power(4, 3);
+
+factorial(0);
+factorial(1);
+factorial(2);
+factorial(5);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +60,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
