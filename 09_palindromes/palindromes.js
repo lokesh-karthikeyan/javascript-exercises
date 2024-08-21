9377db5c4a057df3c9260145bcaf2cb5961f14c4
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-08-21 23:46:23 +0530

    Add function to check palindromes
     + Added conditions to check the array of strings is same if it's reversed.
     + If it's true, then it's a palindrome. Otherwise, it's not.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let arrayString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => (char >= "a" && char <= "z") || Number(char));
 
+  return arrayString.join("") === arrayString.reverse().join("");
 };
+
+palindromes("racecar");
+palindromes("racecar!");
+palindromes("Racecar!");
+palindromes("A car, a man, a maraca.");
+palindromes("Animal loots foliated detail of stool lamina.");
+palindromes("ZZZZ car, a man, a maracaz.");
+palindromes("rac3e3car");
+palindromes("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
