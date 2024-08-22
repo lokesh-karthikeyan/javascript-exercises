7bcbf46939248afdf04500aa482106de258f290b
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-08-22 12:33:50 +0530

    Add f() to calculate fibonacci sequence
     + Created a function to show the fibonacci sequence value of the entered number.
     + If it's a negative value, it's set based on the requirement.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,43 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let position = Number(number);
 
+  if (position === "NaN") {
+    return;
+  }
+
+  if (position < 0) {
+    return "OOPS";
+  }
+
+  if (position >= 0) {
+    let fibonacciArray = [];
+
+    fibonacciArray.push(0);
+    fibonacciArray.push(1);
+
+    if (position === 0) return fibonacciArray[position];
+    if (position === 1) return fibonacciArray[position];
+
+    fibonacciArray.length = position + 1;
+    for (let index = 2; index < fibonacciArray.length; index++) {
+      fibonacciArray[index] =
+        fibonacciArray[index - 1] + fibonacciArray[index - 2];
+    }
+    return fibonacciArray[position];
+  }
 };
+
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(0);
+fibonacci(-25);
+fibonacci("0");
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 
 // Do not edit below this line
 module.exports = fibonacci;
