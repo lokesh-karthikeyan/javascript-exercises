a69cf2fff98daaba2168e0d7ef1c06a12c4f34e9
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-08-22 17:21:11 +0530

    Add functionality to find the oldest
     + The age is derived for each person with "reduce()" method.
     + Filtered that age using "filter()" method and got the oldest person.
    
    Each test scenarios were successfully passed, and the output is similar to the provided solution.
    As per the solution, the solution can be done in a single "reduce()" method.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrOfObj) {
+  let getYear = arrOfObj.reduce((accumulated, item) => {
+    let born = item.yearOfBirth;
+    let died = item.yearOfDeath ?? new Date().getFullYear();
 
+    return accumulated < died - born
+      ? (accumulated = died - born)
+      : accumulated;
+  }, 0);
+
+  let oldest = arrOfObj.filter(
+    (item) =>
+      (item.yearOfDeath ?? new Date().getFullYear()) - item.yearOfBirth ===
+      getYear,
+  );
+
+  return oldest[0];
 };
 
 // Do not edit below this line
