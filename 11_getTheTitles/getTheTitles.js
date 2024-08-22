09af2011aa2e2dd45d0be62ee18bf7d35e4143c6
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-08-22 14:32:22 +0530

    Add f() to create array from array of obj
     + Created a function to create array from the array of objects.
    
    The test scenario were successfully passed, and the output is similar to the provided solution.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arrOfObj) {
+  return arrOfObj.flatMap((item) => item.title);
 };
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
